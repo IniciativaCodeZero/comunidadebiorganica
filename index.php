07efcaea41b410d32db0bdcdb6ffbb24911be17c
Otavio Camargo <ojcamargo@gmail.com>
2019-01-03 16:05:14 -0200

    Layout Home

--- index.php
+++ index.php
@@ -26,20 +26,29 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
       <div class="item active">
-        <img src="https://placehold.it/1200x400?text=Cursos" alt="Image">
+        <img src="imagens/cursos.jpg" alt="cursos" style="width: 90%; height: 400px;" />
         <div class="carousel-caption">
           <h3>Conheça nossos cursos</h3>
-          <p>Saiba mais</p>
+          <p><a href="#">Saiba mais</a></p>
         </div>
       </div>
 
       <div class="item">
-        <img src="https://placehold.it/1200x400?text=Projetos" alt="Image">
+        <img src="imagens/projetos.jpg" alt="projetos" style="width: 90%; height: 400px;" />
         <div class="carousel-caption">
           <h3>Conheça nossos projetos</h3>
-          <p>Saiba mais</p>
+          <p><a href="#">Saiba mais</a></p>
         </div>
       </div>
+
+      <div class="item">
+        <img src="imagens/eventos.jpg" alt="eventos" style="width: 90%; height: 400px;" />
+        <div class="carousel-caption">
+          <h3>Próximos Eventos</h3>
+          <p><a href="#">Saiba mais</a></p>
+        </div>
+      </div>
+
     </div>
 
     <!-- Left and right controls -->
@@ -54,26 +63,58 @@
 </div>
 
 <div class="container text-center">
-  <h3>What We Do</h3><br>
+  <h3>Nossos Projetos</h3><br>
   <div class="row">
     <div class="col-sm-4">
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-      <p>Current Project</p>
+      <img src="imagens/cogumelo.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <p>Biorgânico</p>
     </div>
     <div class="col-sm-4">
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-      <p>Project 2</p>
+      <img src="imagens/floresta.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <p>Agrofloresta</p>
     </div>
     <div class="col-sm-4">
-      <div class="well">
-       <p>Some text..</p>
-      </div>
-      <div class="well">
-       <p>Some text..</p>
-      </div>
+      <img src="imagens/frutas-nativas.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <p>Frutas Nativas</p>
     </div>
   </div>
-</div><br>
+</div>
+
+<div class="container text-center">
+  <h3>Cursos</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=APICULTURA" class="img-responsive" style="width:100%" alt="Image">
+      <p>Apicultura</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=CONSTRUÇÃO" class="img-responsive" style="width:100%" alt="Image">
+      <p>Construção</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=CULINÁRIA" class="img-responsive" style="width:100%" alt="Image">
+      <p>Culinária</p>
+    </div>
+  </div>
+</div>
+
+<div class="container text-center">
+  <h3>Práticas</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=MULTIRÕES" class="img-responsive" style="width:100%" alt="Image">
+      <p>Multirões</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=PESQUISA" class="img-responsive" style="width:100%" alt="Image">
+      <p>Pesquisa</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=VIVÊNCIAS" class="img-responsive" style="width:100%" alt="Image">
+      <p>Vivências</p>
+    </div>
+  </div>
+</div>
 
 <?php include "comum/footer-main.php"; ?>
 
