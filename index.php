655e885107ce60782cb7913e3edfc9b188e174bf
codeatt <sergiojunior2000@hotmail.com>
2019-01-08 22:55:40 -0200

    teste

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
       <div class="item active">
-        <img src="imagens/cursos.jpg" alt="cursos" style="width: 90%; height: 400px;" />
+        <img src="imagens/cursos.jpg" alt="cursos" style="width: 100%; height: 400px;" />
         <div class="carousel-caption">
           <h3>Conheça nossos cursos</h3>
           <p><a href="#">Saiba mais</a></p>
@@ -34,7 +34,7 @@
       </div>
 
       <div class="item">
-        <img src="imagens/projetos.jpg" alt="projetos" style="width: 90%; height: 400px;" />
+        <img src="imagens/projetos.jpg" alt="projetos" style="width: 100%; height: 400px;" />
         <div class="carousel-caption">
           <h3>Conheça nossos projetos</h3>
           <p><a href="#">Saiba mais</a></p>
@@ -42,7 +42,7 @@
       </div>
 
       <div class="item">
-        <img src="imagens/eventos.jpg" alt="eventos" style="width: 90%; height: 400px;" />
+        <img src="imagens/eventos.jpg" alt="eventos" style="width: 100%; height: 400px;" />
         <div class="carousel-caption">
           <h3>Próximos Eventos</h3>
           <p><a href="#">Saiba mais</a></p>
